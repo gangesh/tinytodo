826f3faf396221a658b25723234e3c370334b708
Justin White <kyjus25@gmail.com>
2021-11-20 23:43:50 -0600

    Revert "Alternate compact design"
    This reverts commit 73dc06cc05d3059941e6eabda2eebee60bcfcebb.

--- src/partials/Footer.svelte
+++ src/partials/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="p-4 footer mt-5 bg-blue rounded-tl-2xl rounded-tr-2xl">
+<footer class="p-4 footer bg-blue rounded-tl-2xl rounded-tr-2xl">
     <div class="items-center grid-flow-col">
         <p>Powered by <a class="font-bold" target="_blank" href="http://www.mytinytodo.net">myTinyTodo</a> 2.0.0</p>
     </div> 
--- src/routes/list/[id].svelte
+++ src/routes/list/[id].svelte
@@ -5,7 +5,7 @@
 
 
 <div class="flex w-full">
-    <div class="w-1/4 mr-5 border-r border-blue pr-5">
+    <div class="w-1/4 mr-5">
         <Tabs/>
     </div>
     <div class="flex-1">
