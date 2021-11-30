f10dfa18fd9bfa01cab8d470d2420de736f25ace
Justin White <kyjus25@gmail.com>
2021-11-30 11:44:37 -0600

    Fix indent

--- src/partials/Tooltip.svelte
+++ src/partials/Tooltip.svelte
@@ -5,6 +5,6 @@
 <div class="group-hover:opacity-100 opacity-0 absolute bg-black-lighter text-white px-2 mt-2">
     <div class="w-16 overflow-hidden absolute -top-2 left-10">
         <div class="h-2 w-2 bg-black-lighter rotate-45 transform origin-bottom-left"></div>
-       </div>
+    </div>
     <small>{content}</small>
 </div>
