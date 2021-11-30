96e844d48c35d557b48266a98eb36cd0fe605e97
Justin White <kyjus25@gmail.com>
2021-11-30 13:53:31 -0600

    Filterable rows

---
+++ src/lib/dict.js
@@ -0,0 +1,8 @@
+export const filters = {
+    "ALL": "All Tasks",
+    "TODO": "To Do",
+    "OVERDUE": "Overdue",
+    "NOW": "Today / Tomorrow",
+    "SOON": "Soon",
+    "DONE": "Done"
+}
--- src/partials/Lists.svelte
+++ src/partials/Lists.svelte
@@ -2,12 +2,18 @@
     import MenuOverlay from './MenuOverlay.svelte';
     import { goto } from "$app/navigation";
     import { page } from "$app/stores";
-    import { lists, fetchData, token } from '$lib/stores';
+    import { lists, fetchData, token, filter } from '$lib/stores';
+    import { browser } from "$app/env";
 
     let name = '';
     let token$ = null;
 
     token.subscribe(token => token$ = token);
+
+    const navigate = (id) => {
+        goto(`./${id}`, {replaceState: true});
+        // filter.set('ALL'); // Decided not to reset the filter when the tab is changed
+    }
 
     async function addList() {
         if (name.trim() === '') { return }
@@ -59,7 +65,7 @@
 </script>
 
 <ul class="mt-2.5">
-    <li>
+    <li class="mb-3.5">
         <form on:submit|preventDefault={addList}>
             <input class="border-gray-darkest border w-34 p-1 text-sm rounded-md" type="text" bind:value={name} placeholder="New list">
             <button type="submit" class="hover:opacity-40"><i class="fas fa-plus"/></button>
@@ -68,7 +74,7 @@
     {#each $lists as list, i}
         <li class="flex items-center mr-5 group">
             <div class="flex-1">
-                <a class="block py-1 {$page.path === `/list/${i}` ? 'font-bold' : ''}" href="./{i}">{list.name}</a>
+                <p on:click={() => navigate(i)} class="block cursor-pointer py-1 {$page.path === `/list/${i}` ? 'font-bold' : ''}">{list.name}</p>
             </div>
             <div class="group-hover:opacity-100 opacity-0 h-full">
                 <MenuOverlay dir={'left'} icon={'fas fa-ellipsis-h'}>
@@ -91,4 +97,5 @@
             </div>
         </li>
     {/each}
+    
 </ul>
